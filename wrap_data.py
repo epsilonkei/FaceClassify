62d6e04a83a83ba3771e34084810737be55dc610
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-08-10 22:56:02 +0900

    Fix in target directory in wrap_data.py

--- wrap_data.py
+++ wrap_data.py
@@ -1,25 +1,34 @@
+import argparse
 import os
 import cv2
 import numpy as np
 import json
+from crop_images import target_dir
 
-dir = '../fin_celebA'
 data_dir = 'data'
-train_data = data_dir + '/train_data.npy'
-test_data = data_dir + '/test_data.npy'
-train_name = data_dir + '/train_name.json'
-test_name = data_dir + '/test_name.json'
+
+
+def parser_args():
+    parser = argparse.ArgumentParser(description='Wrap images data to npy and json file')
+    parser.add_argument('--images_dir', '-i', default=target_dir, help='Images directory')
+    parser.add_argument('--data_dir', '-d', default=data_dir, help='Data directory')
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
-    files = os.listdir(dir)
-
+    args = parser_args()
+    train_data = args.data_dir + '/train_data.npy'
+    test_data = args.data_dir + '/test_data.npy'
+    train_name = args.data_dir + '/train_name.json'
+    test_name = args.data_dir + '/test_name.json'
+    files = os.listdir(args.images_directory)
     images = []
     names = []
     for i, file in enumerate(files):
         name, ext = os.path.splitext(file)
         if ext in ['.jpg', '.jpeg', '.png', '.gif']:
-            img = cv2.imread(os.path.join(dir, file))
+            img = cv2.imread(os.path.join(args.images_directory, file))
             images.append(cv2.resize(img, (64, 64)).transpose(2, 0, 1))
             names.append(file)
 
