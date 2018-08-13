de0cd87a235811f0ff4a3410c63c4a944557c3ff
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-08-13 19:15:33 +0900

    Fix bug & minor change in wrap_data.py

--- wrap_data.py
+++ wrap_data.py
@@ -6,6 +6,7 @@ import json
 from crop_images import target_dir
 
 data_dir = 'data'
+train_number = 150000
 
 
 def parser_args():
@@ -22,13 +23,13 @@ if __name__ == '__main__':
     test_data = args.data_dir + '/test_data.npy'
     train_name = args.data_dir + '/train_name.json'
     test_name = args.data_dir + '/test_name.json'
-    files = os.listdir(args.images_directory)
+    files = os.listdir(args.images_dir)
     images = []
     names = []
     for i, file in enumerate(files):
         name, ext = os.path.splitext(file)
         if ext in ['.jpg', '.jpeg', '.png', '.gif']:
-            img = cv2.imread(os.path.join(args.images_directory, file))
+            img = cv2.imread(os.path.join(args.images_dir, file))
             images.append(cv2.resize(img, (64, 64)).transpose(2, 0, 1))
             names.append(file)
 
@@ -37,10 +38,10 @@ if __name__ == '__main__':
 
     images = np.array(images)
     perm = np.random.permutation(len(images))
-    np.save(train_data, images[perm[:80000]])
-    np.save(test_data, images[perm[80000:]])
+    np.save(train_data, images[perm[:train_number]])
+    np.save(test_data, images[perm[train_number:]])
 
     with open(train_name, 'w') as train:
-        json.dump([names[p] for p in perm[:80000]], train)
+        json.dump([names[p] for p in perm[:train_number]], train)
     with open(test_name, 'w') as test:
-        json.dump([names[p] for p in perm[80000:]], test)
+        json.dump([names[p] for p in perm[train_number:]], test)
