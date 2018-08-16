b8e4b3b3c8b00efa8cd0f9b365fde8bce78eec94
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-08-16 13:34:02 +0900

    Add MultiProcessing for crop_images.py

--- crop_images.py
+++ crop_images.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import cv2
+from multiprocessing import Pool
 from detectFaces import getFaces
 import time
 
@@ -25,17 +26,25 @@ def createFolder(directory):
         print ('Error: Creating directory. ' + directory)
 
 
+def cropImage(i, file, args):
+    name, ext = os.path.splitext(file)
+    if ext in ['.jpg', '.jpeg', '.png', '.gif']:
+        face_img = getFaces(os.path.join(args.dataset_dir, file))
+        if len(face_img) > 0:
+            target_file = name + time.strftime("%Y%m%d-%H%M%S") + ext
+            cv2.imwrite(os.path.join(args.target_dir, target_file), face_img[0])
+            #
+    if (i % 200 == 0):
+        print ('{0: 7d} Done'.format(i))
+
+
+def wrapperCropImage(argus):
+    return cropImage(*argus)
+
+
 if __name__ == '__main__':
     args = parser_args()
     files = os.listdir(args.dataset_dir)
     createFolder(args.target_dir)
-    for i, file in enumerate(files):
-        name, ext = os.path.splitext(file)
-        if ext in ['.jpg', '.jpeg', '.png', '.gif']:
-            face_img = getFaces(os.path.join(args.dataset_dir, file))
-            if len(face_img) > 0:
-                target_file = name + time.strftime("%Y%m%d-%H%M%S") + ext
-                cv2.imwrite(os.path.join(args.target_dir, target_file), face_img[0])
-        #
-        if (i % 200 == 0):
-            print ('{0: 7d} Done'.format(i))
+    p = Pool(16)
+    p.map(wrapperCropImage, [[i, file, args] for i, file in enumerate(files)])
